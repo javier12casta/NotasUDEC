fca92bf8b7901e793e2afec2142bd9aff1b520c1
majimenezso <alejo-taizon@hotmail.com>
2021-04-29 09:39:07 -0500

    CAMBIOS

--- src/pages/permissions/permissions.tsx
+++ src/pages/permissions/permissions.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import { useParams } from 'react-router';
 import './permissions.css';
@@ -18,21 +18,83 @@ const permissions: React.FC = () => {
 
       <IonContent fullscreen>
        
-        <IonGrid>
-        <IonRow>
-          <IonCol size="1"></IonCol>
-          {/* login */}
-          <IonCol size="10">
 
-          </IonCol>
-          {/* footer */}
-        <IonCol size="1"></IonCol>
-        </IonRow>          
+       <IonGrid>
+       <IonRow>
+         <IonCol size="2"></IonCol>
+         {/* login */}
+         <IonCol size="10">
 
-        </IonGrid>
+         <IonCard>
+         <IonCardHeader>
+           <IonCardTitle style={{ fontSize: "30px", color: "#000" }}>Permisos</IonCardTitle>
+         </IonCardHeader>
 
-      </IonContent>
-    </IonPage >
+         <IonCardContent>
+
+         <IonItem>
+           <IonLabel position="floating">Certificacion</IonLabel>
+           <IonSelect >
+              <IonSelectOption value="Habilitado">Habilitado</IonSelectOption>
+              <IonSelectOption value="Desabilitado">Desabilitado</IonSelectOption>
+            </IonSelect>
+         </IonItem>
+         <IonItem>
+           <IonLabel position="floating">Documentos De Usuario</IonLabel>
+           <IonSelect >
+              <IonSelectOption value="Habilitado">Habilitado</IonSelectOption>
+              <IonSelectOption value="Desabilitado">Desabilitado</IonSelectOption>
+            </IonSelect>
+         </IonItem>
+
+         <IonItem>
+           <IonLabel position="floating">Autenticacion De Documentos</IonLabel>
+           <IonSelect >
+              <IonSelectOption value="Habilitado">Habilitado</IonSelectOption>
+              <IonSelectOption value="Desabilitado">Desabilitado</IonSelectOption>
+            </IonSelect>
+         </IonItem>
+
+         <IonItem>
+           <IonLabel position="floating">Usuarios</IonLabel>
+           <IonSelect >
+              <IonSelectOption value="Habilitado">Habilitado</IonSelectOption>
+              <IonSelectOption value="Desabilitado">Desabilitado</IonSelectOption>
+            </IonSelect>
+         </IonItem>
+
+         <IonItem>
+           <IonLabel position="floating">Roles</IonLabel>
+           <IonSelect >
+              <IonSelectOption value="Habilitado">Habilitado</IonSelectOption>
+              <IonSelectOption value="Desabilitado">Desabilitado</IonSelectOption>
+            </IonSelect>
+         </IonItem>
+
+         <IonItem>
+           <IonLabel position="floating">Permisos</IonLabel>
+           <IonSelect >
+              <IonSelectOption value="Habilitado">Habilitado</IonSelectOption>
+              <IonSelectOption value="Desabilitado">Desabilitado</IonSelectOption>
+            </IonSelect>
+         </IonItem>
+
+
+         <IonButton expand="block" >
+           Aceptar
+         </IonButton>
+         </IonCardContent>
+       </IonCard>
+
+         </IonCol>
+         {/* footer */}
+       <IonCol size="2"></IonCol>
+       </IonRow>          
+
+       </IonGrid>
+
+     </IonContent>
+   </IonPage >
   );
 };
 
--- src/pages/rols/rols.tsx
+++ src/pages/rols/rols.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import { useParams } from 'react-router';
 import './rols.css';
@@ -17,13 +17,40 @@ const rols: React.FC = () => {
       </IonHeader>
 
       <IonContent fullscreen>
-       
+      
         <IonGrid>
         <IonRow>
           <IonCol size="1"></IonCol>
           {/* login */}
           <IonCol size="10">
 
+          <IonCard>
+          <IonCardHeader>
+            <IonCardTitle style={{ fontSize: "30px", color: "#000" }}>Roles</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+
+          <IonItem>
+            <IonLabel position="floating"> Administrador</IonLabel>
+            <IonSelect >
+              <IonSelectOption value="Habilitado">Habilitado</IonSelectOption>
+              <IonSelectOption value="Desabilitado">Desabilitado</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Usuario</IonLabel>
+            <IonSelect >
+              <IonSelectOption value="Habilitado">Habilitado</IonSelectOption>
+              <IonSelectOption value="Desabilitado">Desabilitado</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+
+          <IonButton expand="block" >
+            Aceptar
+          </IonButton>
+          </IonCardContent>
+        </IonCard>
 
           </IonCol>
           {/* footer */}
